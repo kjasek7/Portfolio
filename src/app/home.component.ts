2523b1bbee3f28be8de064ef12d462cce3b17622
Kamil Jasek <kamil.jasek@tss.black>
2020-03-19 21:41:48 +0100

    a

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -14,57 +14,57 @@ export class HomeComponent implements OnInit {
     this.frontendSkills = [
       {
         skillName: 'html',
-        imagePath: 'assets/icons/frontend/html.png'
+        imagePath: '.assets/icons/frontend/html.png'
       },
       {
         skillName: 'css',
-        imagePath: 'assets/icons/frontend/css.png'
+        imagePath: '.assets/icons/frontend/css.png'
       },
       {
         skillName: 'typescript',
-        imagePath: 'assets/icons/frontend/typescript.png'
+        imagePath: '.assets/icons/frontend/typescript.png'
       },
       {
         skillName: 'angular',
-        imagePath: 'assets/icons/frontend/angular.png'
+        imagePath: '.assets/icons/frontend/angular.png'
       }
     ];
 
     this.backendSkills = [
       {
         skillName: 'java',
-        imagePath: 'assets/icons/backend/java.png'
+        imagePath: '.assets/icons/backend/java.png'
       },
       {
         skillName: 'spring',
-        imagePath: 'assets/icons/backend/spring.png'
+        imagePath: '.assets/icons/backend/spring.png'
       },
       {
         skillName: 'hibernate',
-        imagePath: 'assets/icons/backend/hibernate.png'
+        imagePath: '.assets/icons/backend/hibernate.png'
       },
       {
         skillName: 'sql',
-        imagePath: 'assets/icons/backend/sql.png'
+        imagePath: '.assets/icons/backend/sql.png'
       }
     ];
 
     this.toolsSkills = [
       {
         skillName: 'intelij',
-        imagePath: 'assets/icons/tools/intelij.png'
+        imagePath: '.assets/icons/tools/intelij.png'
       },
       {
         skillName: 'git',
-        imagePath: 'assets/icons/tools/git.png'
+        imagePath: '.assets/icons/tools/git.png'
       },
       {
         skillName: 'jira',
-        imagePath: 'assets/icons/tools/jira.png'
+        imagePath: '.assets/icons/tools/jira.png'
       },
       {
         skillName: 'docker',
-        imagePath: 'assets/icons/tools/docker.png'
+        imagePath: '.assets/icons/tools/docker.png'
       }
     ];
   }
